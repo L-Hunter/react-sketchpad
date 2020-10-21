f1d0719df08eaa880cb7eaa22405d9620fb08b3f
TheButlerDidIt <labutler@gmail.com>
2020-10-21 14:02:49 -0400

    canvas and controls render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const cells = [];
+  for (let x = 0; x < 3990; x++) {
+    cells.push(<div className="cell" key={x}></div>)
+  }
+
+  const colors = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']
+  const controls = [];
+  for (let color of colors) {
+    controls.push(<button id={color} className={color} key={color}>{color}</button>)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="pad">
+        <h1>Sketchpad in React</h1>
+        <div className="buttons">{controls}</div>
+        <div id="pad">{cells}</div>
     </div>
   );
 }
