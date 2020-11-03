dbda78583da8ed73bcecd50c7c027447db614fe2
TheButlerDidIt <labutler@gmail.com>
2020-11-03 13:06:56 -0500

    improves drawing with mouse controls

--- src/App.js
+++ src/App.js
@@ -3,35 +3,49 @@ import './App.css';
 
 function App() {
 
-  let bgColor = 'purple';
+  // this should be changed by changeColor
+  let bgColor = 'black';
+  let isDrawing = false;
 
-  const handleClick = (e) => {
-    console.log("this is working fine");
+  const startDrawing = () => {
+    isDrawing = true;
+  } 
+
+  const keepDrawing = (e) => {
     e.preventDefault();
-    e.target.style.backgroundColor = bgColor;
-    console.log(e.target);
+    if (isDrawing) {
+      e.target.style.backgroundColor = bgColor;
+    }
   }
 
-  const handleColor = () => {
-    console.log('set the bgColor')
+  const stopDrawing = () => {
+    isDrawing = false;
+  }
+
+  const changeColor = () => {
+    bgColor = 'yellow';
+    console.log(`A click! is this doing anything? what's the ${bgColor}`)
   }
 
   const cells = [];
   for (let x = 0; x < 3990; x++) {
-    cells.push(<div className="cell" key={x} onMouseMove={ handleClick }></div>)
+    cells.push(<div className="cell" key={x} onMouseDown={ startDrawing } onMouseMove={ keepDrawing }  onMouseUp={ stopDrawing }></div>)
   }
 
   const colors = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']
   const controls = [];
   for (let color of colors) {
-    controls.push(<button id={color} className={color} key={color} onClick={ handleColor }>{color}</button>)
+    controls.push(<button id={color} className={color} key={color} onClick={ changeColor }>{color}</button>)
   }
 
   return (
-    <div className="App" id="pad">
+    <div className="App">
+      
+      <div id="pad">
         <h1>Sketchpad in React</h1>
         <div className="buttons" >{controls}</div>
         <div id="pad">{cells}</div>
+      </div>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className="menu">
+      <h3><a href="https://l-hunter.github.io/">Laura Hunter</a></h3>
+    </div>
+  )
+}
+
+export default Nav;
