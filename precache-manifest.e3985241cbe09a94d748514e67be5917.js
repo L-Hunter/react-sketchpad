92415d55644af6fad12a2041a767a795162dc15f
TheButlerDidIt <labutler@gmail.com>
2020-11-03 20:43:35 -0500

    Updates

---
+++ precache-manifest.e3985241cbe09a94d748514e67be5917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a2cbc10a99ebe41fef58678dd3f136",
+    "url": "/react-sketchpad/index.html"
+  },
+  {
+    "revision": "5d24b6c49065e5367f89",
+    "url": "/react-sketchpad/static/css/main.3a25c800.chunk.css"
+  },
+  {
+    "revision": "8527cb94059c51bb1e9d",
+    "url": "/react-sketchpad/static/js/2.7a56cf90.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-sketchpad/static/js/2.7a56cf90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d24b6c49065e5367f89",
+    "url": "/react-sketchpad/static/js/main.915bdc1c.chunk.js"
+  },
+  {
+    "revision": "f2f41f32ba4440654708",
+    "url": "/react-sketchpad/static/js/runtime-main.bb6d2581.js"
+  }
+]);
